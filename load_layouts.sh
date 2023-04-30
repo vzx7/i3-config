fc7a8768677904c45e3a5a940936f641086eda50
vzx7 <coder.vz@gmail.com>
2023-04-30 19:46:36 +0300

    edit comments

--- load_layouts.sh
+++ load_layouts.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
-# We append the saved layout of workspace 1 to workspace M
+# We append the saved layout of workspace 1 to workspace 1
 i3-msg "workspace 1; append_layout /home/zx/.config/i3/workspace-1.json"
 
-# And finally we fill the containers with the programs they had
+# We fill the containers with the programs they had
 (code-oss &)
 
-# We append the saved layout of workspace 2 to workspace M
+# We append the saved layout of workspace 2 to workspace 2
 i3-msg "workspace 2; append_layout /home/zx/.config/i3/workspace-2.json"
 
-# And finally we fill the containers with the programs they had
+# We fill the containers with the programs they had
 (firefox &)
 (opera &)
 
-# We append the saved layout of workspace 3 to workspace M
+# We append the saved layout of workspace 3 to workspace 2
 i3-msg "workspace 3; append_layout /home/zx/.config/i3/workspace-3.json"
 
 # And finally we fill the containers with the programs they had
